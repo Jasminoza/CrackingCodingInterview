b3a466ee7f23f841fa2ad5990057f286bbc4a451
Jasminoza <testbot91@gmail.com>
2022-02-16 19:29:15 +0300

    First exercise.

--- src/First.java
+++ src/First.java
@@ -9,21 +9,21 @@ public class First {
     public static void main(String[] args) {
         int m = 3;
         int n = 5;
-
         System.out.println(Arrays.toString(createRandomArr(m, n))); // выводим m чисел из массива размером n.
-
+        m = 7; n =20;
+        System.out.println(Arrays.toString(createRandomArr(m, n))); // выводим m чисел из массива размером n.
     }
 
     public static int[] createRandomArr(int m, int n) {
         int[] arr = new int[n];
         int[] arr2 = new int[m];
         for (int x = 0; x < m; x++) {
-            arr[x] = (int) (Math.random() * 10 - 1);
+            arr[x] = (int) (Math.random() * 100 - 1);
         }
         System.out.println("Исходный массив: " + Arrays.toString(arr));
 
         for (int y = 0; y < m; y++) {
-            arr2[y] = arr[(int) (Math.random() * 3)];
+            arr2[y] = arr[(int) (Math.random() * m)];
         }
         System.out.print("Массив из случайных m чисел: ");
         return arr2;
