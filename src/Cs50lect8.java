cf421c3291333306e0f9b64a6ea06999006fad7d
Jasminoza <testbot91@gmail.com>
2022-02-18 08:53:11 +0300

    123

---
+++ src/Cs50lect8.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Cs50lect8 {
+    public static void main(String[] args) {
+        int[] arr = {4, 8, 6, 2, 1, 7, 5, 3};
+        System.out.println(Arrays.toString(doSort(arr)));
+    }
+
+    public static int[] doSort(int[] arr) {
+
+        return;
+    }
+
+    public static int[] sortTwoInt(int[] arrTwoElem) {
+
+        return arrTwoElem;
+    }
+}
